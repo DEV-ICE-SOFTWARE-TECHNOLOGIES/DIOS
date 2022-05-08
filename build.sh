d9a77397537ce68064c06d62e208e3d3fec58719
Marius Kopp <mariuskopp517@gmail.com>
2022-05-08 13:14:54 +0200

    220508
    Almost there...

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 set -eu
 
 # ----------------------------------------------------------------------
-# Variables that can be overriden by caller
+# Variables
 # ----------------------------------------------------------------------
-SOURCE=${SOURCE:-~/dios/dios/source}
+SOURCE=~/dios
 APK_DIR=${APK_DIR:-~/dios/dios/apk}
 NAME=${NAME:-~M1U5T0N3}
 EMAIL=${EMAIL:-~mariuskopp517@gmail.com}
@@ -14,7 +14,7 @@ LUNCH_CHOICE=aosp_j9210-userdebug
 
 _show_help() {
     echo "Usage:"
-    echo "  $_shell_script [-b <manifest_branch> [-k|--keep-local]] [-h|--help]"
+    echo "  $_shell_script [-a|--aosp] [-h|--help]"
     echo ""
     echo "AUTOMATED D!OS BUILD SCRIPT"
     echo ""
@@ -23,105 +23,77 @@ _show_help() {
     echo "  - Deleting Files"
     echo "  - Creating Files"
     echo "  - Eating much Storage"
-    echo "  - Git hard resets"
-    echo "  - Git checkouts"
-    echo "  ...Therefore the Script must not be used in a developers AOSP Tree with changed Files"
-    echo "  and/or local Commits. Both might get lost when running this Scripts!"
     echo ""
-    echo "Options:"
-    echo "  -b <manifest_branch>    Switches the Repo to the specified manifest_branch, e.g. android-12.1.0_r5"
-    echo "  -k|--keep-local         Keeps the branch for the local manifests Repo when switching Branches"
-    echo "  -h|--help               Display this Help"
-    echo "  -x|--exclude-gapps      Excludes GApps from the build and implicitly removes the GApps"
-    echo "                          Repos from the Source Tree"
+    echo "OPTIONS:"
+    echo "  -a|--aosp               Build only AOSP, e.g. android-12.1.0_r5"
+    echo "  -h|--help               Displays this Help"
     echo ""
-    echo "Script variables:"
-    echo "  SOURCE          D!OS root folder"
-    echo "                  Default: ~/dios/dios/source"
-    echo "  APK_DIR         currently not used"
-    echo "                  Default: ~/dios/dios/source"
-    echo "  LUNCH_CHOICE    e.g. aosp_h3113-userdebug, aosp_h9436-userdebug,..."
+    echo "SCRIPT VARIABLES:"
+    echo "  SOURCE          Default: ~/dios"
+    echo ""
+    echo "  APK_DIR         Default: ~/dios/dios/apk"
+    echo ""
+    echo "  LUNCH_CHOICE    E.g. aosp_h3113-userdebug, aosp_h9436-userdebug,..."
     echo "                  Default: not set"
     echo ""
     echo "To pass the Variables to the Script use env, e.g. for pioneer use following Command:"
     echo "  env LUNCH_CHOICE=aosp_h3113-userdebug ./$_shell_script"
 }
 
-_init() {
-  sudo apt-get purge openjdk-* icedtea-* icedtea6-* || true 
-  sudo apt-get update
-  sudo apt-get install openjdk-11-jdk
-  sudo apt-get install bison g++-multilib git gperf libxml2-utils make zlib1g-dev zip liblz4-tool libncurses5 libssl-dev bc flex curl python-is-python3 ccache
+# ----------------------------------------------------------------------
+# INITIALIZING
+# ----------------------------------------------------------------------
+_init_dios() {
+  #sudo apt-get purge openjdk-* icedtea-* icedtea6-* || true 
+  #sudo apt-get update
+  #sudo apt-get install openjdk-11-jdk
+  #sudo apt-get install bison g++-multilib git gperf libxml2-utils make zlib1g-dev zip liblz4-tool libncurses5 libssl-dev bc flex curl python-is-python3 ccache
 
-  mkdir ~/bin || true 
-  curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
-  chmod a+x ~/bin/repo
+  #mkdir ~/bin || true 
+  #curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+  #chmod a+x ~/bin/repo
 
-  echo '' >> ~/.bashrc
-  echo 'export PATH=~/bin:$PATH' >> ~/.bashrc
-  echo 'export USE_CCACHE=1' >> ~/.bashrc
-  echo 'export CCACHE_EXEC=/usr/bin/ccache' >> ~/.bashrc
-  echo 'export CCACHE_DIR=~/.ccache' >> ~/.bashrc
-  wait
-  source ~/.bashrc
-  ccache -M 300G -F 0
+  #echo '' >> ~/.bashrc
+  #echo 'export PATH=~/bin:$PATH' >> ~/.bashrc
+  #echo 'export USE_CCACHE=1' >> ~/.bashrc
+  #echo 'export CCACHE_EXEC=/usr/bin/ccache' >> ~/.bashrc
+  #echo 'export CCACHE_DIR=~/.ccache' >> ~/.bashrc
+  #wait
+  #source ~/.bashrc
+  #ccache -M 300G -F 0
 
-  if [ -d /mnt/ccache ]; then
-    sudo mount --bind $USERNAME/.ccache /mnt/ccache
-  else
-    sudo mkdir /mnt/ccache
-    sudo mount --bind $USERNAME/.ccache /mnt/ccache
-  fi
+  #if [ -d /mnt/ccache ]; then
+  #  sudo mount --bind $USERNAME/.ccache /mnt/ccache
+  #else
+  #  sudo mkdir /mnt/ccache
+  #  sudo mount --bind $USERNAME/.ccache /mnt/ccache
+  #fi
 
-  if [ -d ~/dios/.repo ]; then
-    rm -r ~/dios/.repo/*
-  fi
+  #if [ -d ~/dios/.repo ]; then
+  #  rm -r ~/dios/.repo/*
+  #fi
 
-  if [ -d ~/dios/.git ]; then
-    rm -r ~/dios/.git/*
-  fi
+  #if [ -d ~/dios/.git ]; then
+  #  rm -r ~/dios/.git/*
+  #fi
 
-  repo init -u https://android.googlesource.com/platform/manifest -b android-12.1.0_r5
-  cd .repo
-  git clone https://github.com/DEV-ICE-TECHNOLOGIES/local_manifests
-  cd local_manifests
-  git checkout
-  cd ../..
-  repo sync -j$(nproc) && ./repo_update.sh -j$(nproc)
+  #repo init -u https://android.googlesource.com/platform/manifest -b android-12.1.0_r5
+  #cd .repo
+  #git clone https://github.com/DEV-ICE-TECHNOLOGIES/local_manifests
+  #cd local_manifests
+  #git checkout
+  #cd ../..
+  repo sync -j$(nproc) --force-sync && ./repo_update.sh -j$(nproc)
+
+  _start_building
 }
 
-if [ -d dios/dios/ ]; then
-    _pick_pr
-else
-    _init
-    _init_gapps
-fi
-
-_pick_pr() {
-    local _remote=$1
-    local _pr_refspec=$2
-    local _commits=${3:-1}
-    local _max_commits=${4:-$_commits}
-    local _excluded_commits=${5:-}
-    local _index=$(($_commits - 1))
-    local _count=0
-    local _current_commit
-
-    git fetch $_remote $_pr_refspec
-
-    while [ $_index -ge 0 -a $_count -lt $_max_commits ]; do
-        _current_commit=`git log --pretty=format:%h FETCH_HEAD~$_index|head -1`
-        if echo $_excluded_commits | grep $_current_commit; then
-            echo "skip commit"
-        else
-            git cherry-pick --allow-empty -Xtheirs --no-edit FETCH_HEAD~$_index
-        fi
-        _index=$(($_index - 1))
-        _count=$(($_count + 1))
-    done
-}
-
-_put_gapps_apk() {
+# ----------------------------------------------------------------------
+# MAIN SCRIPT
+# ----------------------------------------------------------------------
+_gapps_fork() {
+    sh ./gapps_fork.sh
+    wait
     local _apk_name=$1
     local _target_dir=$2
     local _version=`aapt dump badging $APK_DIR/$_apk_name |grep versionCode=|sed "s#.*versionCode='\([[:digit:]]*\).*#\1#1"`
@@ -130,68 +102,8 @@ _put_gapps_apk() {
     cp $APK_DIR/$_apk_name $_target_dir/$_version.apk
 }
 
-_clean()  {
-    pushd .repo/manifests
-        git clean -d -f
-        git checkout .
-        git pull
-    popd
-
-    pushd .repo/local_manifests
-        _local_manifests_branch=$(git symbolic-ref -q HEAD)
-        _local_manifests_branch=${_local_manifests_branch##refs/heads/}
-        _local_manifests_branch=${_local_manifests_branch:-HEAD}
-
-        git clean -d -f
-        git fetch
-        git reset --hard origin/$_local_manifests_branch
-    popd
-
-    if [ -d kernel/sony/msm-4.9 ]; then
-        rm -r kernel/sony/msm-4.9
-    fi
-
-    if [ -d kernel/sony/msm-5.4 ]; then
-        rm -r kernel/sony/msm-5.4
-    fi
-
-    if [ -d hardware/qcom/sm8150 ]; then
-        rm -r hardware/qcom/sm8150
-    fi
-
-    if [ -d hardware/qcom/sm8250 ]; then
-        rm -r hardware/qcom/sm8250
-    fi
-
-    if [ -d hardware/qcom/sm8350 ]; then
-        rm -r hardware/qcom/sm8350
-    fi
-
-    if [ -d dios/dios/ ]; then
-        rm -r dios/dios/
-    fi
-
-    if [ -h vendor/qcom/opensource/Android.bp ]; then
-        rm vendor/qcom/opensource/Android.bp
-    fi
-
-    for _path in \
-        device/sony/$_platform \
-        device/sony/common \
-        device/sony/sepolicy \
-        vendor/oss/transpower
-    do
-        if [ -d $_path ]; then
-            pushd $_path
-                git clean -d -f
-                git reset --hard m/$_current_branch
-            popd
-        fi
-    done
-}
-
 _init_gapps() {
-    if $_exclude_gapps; then
+    if $_aosp; then
         return
     fi
 
@@ -206,19 +118,8 @@ EOF
     popd
 }
 
-_repo_switch() {
-    repo sync -j$(nproc) --force-sync
-    repo init -b $_new_branch
-
-    if [ "$_keep_local" = "false" ]; then
-        pushd .repo/local_manifests
-            git checkout $_new_branch
-        popd
-    fi
-}
-
 _repo_update() {
-    ./repo_update.sh
+    ./repo_update.sh -j$(nproc)
 }
 
 _post_update() {
@@ -227,7 +128,7 @@ _post_update() {
 
 _customize_build() {
     mkdir dios/dios
-    if ! $_exclude_gapps; then
+    if ! $_aosp; then
         cat >dios/dios/dios.mk <<EOF
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
@@ -244,7 +145,7 @@ EOF
 }
 
 _make() {
-    if [ -n "$_new_branch" ]; then
+    if [ -n "$_aosp" ]; then
         make clean
     else
         make installclean
@@ -254,42 +155,21 @@ _make() {
 }
 
 _build() {
-    _clean
+    _gapps_fork
     _init_gapps
     _repo_update
     _post_update
+    _customize_build
     _make
 }
 
-_switch_branch() {
-    _clean
-    _init_gapps
-    _repo_switch
-}
-
-# ----------------------------------------------------------------------
-# Main
-# ----------------------------------------------------------------------
-
-cd $SOURCE
-
 declare _shell_script=${0##*/}
-declare _new_branch=""
-declare _keep_local="false"
-declare _exclude_gapps="false"
+declare _aosp="false"
 
 while (( "$#" )); do
     case $1 in
-        -b)
-            _new_branch=$2
-            shift 2
-            ;;
-        -k|--keep-local)
-            _keep_local="true"
-            shift
-            ;;
-        -x|--exclude-gapps)
-            _exclude_gapps="true"
+        -a|--aosp)
+            _aosp="true"
             shift
             ;;
         -h|--help)
@@ -303,10 +183,13 @@ while (( "$#" )); do
     esac
 done
 
-if [ -z "$_new_branch" -a "$_keep_local" = "true" ]; then
-    echo "-k|--keep-local can only be used with -b"
-    echo "For help use $_shell_script -h"
-fi
+# ----------------------------------------------------------------------
+# START BUILDING
+# ----------------------------------------------------------------------
+
+_start_building() {
+
+cd $SOURCE
 
 set +u # prevent following android calls from failing because of unset variables
 . build/envsetup.sh
@@ -315,10 +198,14 @@ declare _device=`get_build_var PRODUCT_DEVICE 2>/dev/null`
 declare _platform=`get_build_var PRODUCT_PLATFORM 2>/dev/null`
 set -u
 
-_current_branch=`cat .repo/manifests/default.xml|grep default\ revision|sed 's#^.*refs/tags/\(.*\)"#\1#1'`
-
-if [ -n "$_new_branch" ]; then
-    _switch_branch
-fi
-
 _build
+}
+
+# ----------------------------------------------------------------------
+# READY?
+# ----------------------------------------------------------------------
+if [ -d ~/dios/dios ]; then
+    _start_building
+else
+    _init_dios
+fi
